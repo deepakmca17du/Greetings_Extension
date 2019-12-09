9d8275af7b4f3fdaffe912fd7622be3617522e95
Deepak Yadav <31646033+deepakmca17du@users.noreply.github.com>
2019-12-09 20:24:10 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,12 +1,14 @@
 var d = new Date().getHours();
 		
-if (d < 12 ) {
+if (d >= 4 && d < 12) {
 	alert(document.getElementById("demo").innerHTML = "Good morning!");
 }
-if (d > 12 && d < 16 ) {
+if (d >= 12 && d < 16 ) {
 	alert(document.getElementById("demo").innerHTML = "Good afternoon!");
 }
-if (d > 16 ) {
+if (d >= 16 && d < 23 ) {
 	alert(document.getElementById("demo").innerHTML = "Good evening!");
-
+}
+if (d >= 23 || d < 4 ) {
+	alert(document.getElementById("demo").innerHTML = "Good night!");
 }
